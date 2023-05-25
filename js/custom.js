99cb0ad045934f5831faa4a42d727fcb71836de0
rakm9 <134600667+rakm9@users.noreply.github.com>
2023-05-26 03:27:11 +1000

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,54 @@
+
+$(function () {
+
+  // MENU
+  $('.navbar-collapse a').on('click', function () {
+    $(".navbar-collapse").collapse('hide');
+  });
+
+  // AOS ANIMATION
+  AOS.init({
+    disable: 'mobile',
+    duration: 800,
+    anchorPlacement: 'center-bottom'
+  });
+
+
+  // SMOOTHSCROLL NAVBAR
+  $(function () {
+    $('.navbar a, .hero-text a').on('click', function (event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top - 49
+      }, 1000);
+      event.preventDefault();
+    });
+  });
+});
+
+
+
+// SLIDESHOW
+$(function () {
+  // Existing code...
+
+  // Slideshow initialization code
+  const swiper = new Swiper('.swiper', {
+    autoplay: {
+      delay: 9000,
+      disableOnInteraction: false,
+    },
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+});
+
+
+
